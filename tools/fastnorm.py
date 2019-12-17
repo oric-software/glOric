5021e3ace310c25f253c3d71a90fd9941be785c2
jbperin <jbperin@users.noreply.github.com>
2019-12-17 23:25:38 +0100

    find a way to reduce inaccuracy of norm computation by using biplanar approximation

--- tools/fastnorm.py
+++ tools/fastnorm.py
@@ -1,23 +1,34 @@
 import math
 
 tabmult = [round (  i*(math.sqrt(2)-1)) for i in range (128) ]
+
 def norm (x,y):
     return math.sqrt(x**2 + y**2)
     
-def fakenorm (x,y):
-    if x>y:
-        return x+tabmult[y]
+def fakenorm (px,py):
+    ax, ay = abs(px), abs(py)
+    if ax==ay:
+       return math.sqrt(2) * ax
+    elif ax > ay:
+        x, y = ax, ay
     else:
-        return y+tabmult[x]
+        x, y = ay, ax
+    if y == x/2:
+        return x*(math.sqrt(5)/2)
+    elif y > x/2 :
+        N = (math.sqrt(5)-math.sqrt(2))*x + (2*math.sqrt(2) - math.sqrt(5))*y
+    else:
+        N = x+(math.sqrt(5)/2 - 1)*y
+    return N
     
 def main():
-    x,y = 2,4
+    x,y = 8,8
     err = norm(x,y) - fakenorm(x,y)
     print ("%d %d err = %d"%(x,y,err))
     
-    for i in range (0,len(tabmult),8):
-            print (', '.join(tabmult[i:i+8]))
-            if i%8 == 0 : print ("\n")
+    #for i in range (0,len(tabmult),8):
+    #        print (', '.join(tabmult[i:i+8]))
+    #        if i%8 == 0 : print ("\n")
         
 if __name__ == '__main__':
     main()
