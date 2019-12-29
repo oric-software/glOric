e807224fee9aacb84bf1f993bf9d99ea5fc84cde
jbperin <jbperin@users.noreply.github.com>
2019-12-29 10:15:09 +0100

    attempt to use cl65 + start thinking of clipping

--- src/glOric.asm
+++ src/glOric.asm
@@ -4,10 +4,27 @@
 .import popa
 
 
+
+;; Point 3D Coordinates
+_PointX:		.word 0
+_PointY:		.word 0
+_PointZ:		.word 0
+
+_Norm:          .word 0
+
+;; Point 2D Projected Coordinates
+_ResX:			.byte 0	
+_ResY:			.byte 0
+
+.segment  "ZEROPAGE"
+
 ptrpt3:				.word 0
 ptrpt2:				.word 0
 nbPoints:			.byte 0
 opts:				.byte 0
+
+
+			.segment "CODE"
 
 ;---------------------------------------------------------------------------------
 ;void glProject (char *tabpoint2D, char *tabpoint3D, unsigned char nbPoints, unsigned char opts);
@@ -25,6 +42,75 @@ opts:				.byte 0
 	sta ptrpt2
 	stx ptrpt2+1
 
+    ldx nbPoints
+    dex
+    txa ; ii = nbPoints - 1
+    asl
+    asl ; ii * SIZEOF_3DPOINT (4)
+    clc
+    adc #$03
+    tay
+    
+    ldx nbPoints
+    dex
+    txa ; ii = nbPoints - 1
+    asl
+    asl ; ii * SIZEOF_2DPOINT (4)
+    clc
+    adc #$03
+    tax
+    
+dofastprojloop:
+;;          Status = points3d[ii*SIZEOF_3DPOINT + 3]
+        dey
+;;  		PointZ = points3d[ii*SIZEOF_3DPOINT + 2];
+        lda (ptrpt3),y
+        sta _PointZ
+        dey
+;;  		PointY = points3d[ii*SIZEOF_3DPOINT + 1];
+        lda (ptrpt3),y
+        sta _PointY
+        dey
+;;  		PointX = points3d[ii*SIZEOF_3DPOINT + 0];
+        lda (ptrpt3),y
+        sta _PointX
+        dey
+
+;;  		project();
+        ;;jsr _project
+        
+;;  		points2d[ii*SIZEOF_2DPOINT + 1] = ResY;
+        tya
+        pha
+        txa
+        tay
+        
+        lda _Norm+1
+        sta (ptrpt2), y
+        dey 
+
+        lda _Norm
+        sta (ptrpt2), y
+        dey
+
+        lda _ResY
+        sta (ptrpt2), y
+;;  		points2d[ii*SIZEOF_2DPOINT + 0] = ResX;
+        dey
+        lda _ResX
+        sta (ptrpt2), y
+
+        tya
+        tax
+        pla
+;;  	}
+    dex
+    txa
+    cmp #$FF
+    bne dofastprojloop 
+dofastprojdone:
+	
+	
     rts
 .endproc
 
--- tools/gloric.py
+++ tools/gloric.py
@@ -80,6 +80,41 @@ def project ():
         print (str (p), str(p2d), ang1, dist, ang2)
         points2.append (p2d)
 
+
+def clip(subjectPolygon, clipPolygon):
+   def inside(p):
+      return(cp2[0]-cp1[0])*(p[1]-cp1[1]) > (cp2[1]-cp1[1])*(p[0]-cp1[0])
+ 
+   def computeIntersection():
+      dc = [ cp1[0] - cp2[0], cp1[1] - cp2[1] ]
+      dp = [ s[0] - e[0], s[1] - e[1] ]
+      n1 = cp1[0] * cp2[1] - cp1[1] * cp2[0]
+      n2 = s[0] * e[1] - s[1] * e[0] 
+      n3 = 1.0 / (dc[0] * dp[1] - dc[1] * dp[0])
+      return [(n1*dp[0] - n2*dc[0]) * n3, (n1*dp[1] - n2*dc[1]) * n3]
+ 
+   outputList = subjectPolygon
+   cp1 = clipPolygon[-1]
+ 
+   for clipVertex in clipPolygon:
+      cp2 = clipVertex
+      inputList = outputList
+      outputList = []
+      s = inputList[-1]
+ 
+      for subjectVertex in inputList:
+         e = subjectVertex
+         if inside(e):
+            if not inside(s):
+               outputList.append(computeIntersection())
+            outputList.append(e)
+         elif inside(s):
+            outputList.append(computeIntersection())
+         s = e
+      cp1 = cp2
+   return(outputList)
+
+
 def initScreen (): screen = [[' ' for i in range(LE)] for j in range(HE)]
 
 def printScreen ():
@@ -97,6 +132,12 @@ def main():
         print ("face",pA, pB, pC, points2[pA], points2[pB], points2[pC])
         drawFace( points2[pA][0],  points2[pA][1],  points2[pB][0],  points2[pB][1],  points2[pC][0],  points2[pC][1])
     printScreen ()
+
+    subjectPolygon =  [ ( -5 , 5 ) , ( 20 , -5 ) , ( 45, 5 ) ] 
+    #clipPolygon =  [ ( 100 , 100 ) , ( 300 , 100 ) , ( 300 , 300 ) , ( 100 , 300 ) ]
+    clipPolygon =  [ ( 0 , 0 ) , ( 39 , 0 ) , ( 39 , 26 ) , ( 0 , 26 ) ] 
+    lPts = clip(subjectPolygon, clipPolygon)
+    print (lPts)
 
 if __name__ == '__main__':
     main()
