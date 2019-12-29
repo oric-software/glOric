1cd7e19aaa7887881bed0c87313c70b1412ba03c
jbperin <jbperin@users.noreply.github.com>
2019-12-29 17:44:09 +0100

    use defined path for Oricutron

--- src/caBuild.bat
+++ src/caBuild.bat
@@ -2,8 +2,8 @@ set PATH_TO_ORICUTRON=C:\Users\Public\Oricutron
 
 ca65 -tatmos glOric.asm -o glOric.o  --include-dir .
 rem cl65 -C linker.cfg  -tatmos demo.c glOric.o -o toto.tap
-cl65 -C linker.cfg  -tatmos demo.c -o toto.tap
-copy toto.tap C:\Users\Public\Oricutron\tapes
+cl65   -tatmos demo.c -o toto.tap
+copy toto.tap %PATH_TO_ORICUTRON%\tapes
 
 PUSHD %PATH_TO_ORICUTRON%
 .\oricutron.exe -t tapes\toto.tap
