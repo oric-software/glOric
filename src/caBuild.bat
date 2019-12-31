f6adc529a26c784d45dd3a3d140ba5d350d21bd5
jbperin <jbperin@users.noreply.github.com>
2019-12-31 16:12:42 +0100

    turns target to telestrat in order to run on ORIX

--- src/caBuild.bat
+++ src/caBuild.bat
@@ -1,12 +1,15 @@
 set PATH_TO_ORICUTRON=C:\Users\Public\Oricutron
+set PLATFORM=telestrat
 
-ca65 -tatmos glOric.asm -g -l glOric.lst -o glOric.o  --include-dir .
-ar65 r glOric.lib  glOric.o
+ca65 -t%PLATFORM% glOric.asm -g -l glOric.lst -o glOric.o  --include-dir .
+ca65 -t%PLATFORM% raster\line.asm -g -l line.lst -o line.o  --include-dir .
+ar65 r glOric.lib line.o glOric.o
 cc65 demo.c -g -o demo.asm
-ca65 -tatmos demo.asm -g -l demo.lst -o demo.o  --include-dir .
-cl65 -v -vm -m demo.map -Ln demo.vice -g -tatmos demo.o glOric.lib  -o demoGlOric.tap
+ca65 -t%PLATFORM% demo.asm -g -l demo.lst -o demo.o --include-dir .
+cl65 -v -vm -m demo.map -Ln demo.vice -g -t%PLATFORM% demo.o glOric.lib  -o demoGlOric.tap
 copy demoGlOric.tap %PATH_TO_ORICUTRON%\tapes
+copy demoGlOric.tap %PATH_TO_ORICUTRON%\usbdrive\bin\d
 
-PUSHD %PATH_TO_ORICUTRON%
-.\oricutron.exe -t tapes\demoGlOric.tap
-POPD
+REM PUSHD %PATH_TO_ORICUTRON%
+REM START .\oricutron.exe
+REM POPD
--- src/demo.c
+++ src/demo.c
@@ -43,7 +43,7 @@ int main ()
 
 	val = une_fonction();
 	glProject (points2d, points3d, 1, 0);
-	printf("Press a key to return to basic %d\n", val);
+	printf("Value returned by function: %d\n", val);
 	cgetc();
 
 	tgi_done();
