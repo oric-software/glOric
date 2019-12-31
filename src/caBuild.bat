f986a1d08f06ebcf3c5c03caedf137e91a5f4f9b
jbperin <jbperin@users.noreply.github.com>
2019-12-31 10:21:32 +0100

    create lib with ar65

--- src/caBuild.bat
+++ src/caBuild.bat
@@ -1,7 +1,10 @@
 set PATH_TO_ORICUTRON=C:\Users\Public\Oricutron
 
-ca65 -tatmos glOric.asm -l glOric.lst -o glOric.o  --include-dir .
-cl65 -v -vm -m demo.map -Ln demo.vice -tatmos demo.c glOric.o  -o demoGlOric.tap
+ca65 -tatmos glOric.asm -g -l glOric.lst -o glOric.o  --include-dir .
+ar65 r glOric.lib  glOric.o
+cc65 demo.c -g -o demo.asm
+ca65 -tatmos demo.asm -g -l demo.lst -o demo.o  --include-dir .
+cl65 -v -vm -m demo.map -Ln demo.vice -g -tatmos demo.o glOric.lib  -o demoGlOric.tap
 copy demoGlOric.tap %PATH_TO_ORICUTRON%\tapes
 
 PUSHD %PATH_TO_ORICUTRON%
