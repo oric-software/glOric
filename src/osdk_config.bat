a73fcb8ab4ea82dc4b628279ad2a525a096fe726
jbperin <jbperin@users.noreply.github.com>
2020-01-27 22:08:33 +0100

    fix build script

--- src/osdk_config.bat
+++ src/osdk_config.bat
@@ -10,17 +10,16 @@ REM SET OSDKLINK=-B
 
 SET OSDKFILE=main
 
- 
+
 SET OSDKFILE=%OSDKFILE%  glOric kernel
-SET OSDKFILE=%OSDKFILE% math\atan2 math\div math\norm math\root math\square 
-SET OSDKFILE=%OSDKFILE% raster\buffer raster\display raster\filler raster\line raster\line8 raster\fill8
-SET OSDKFILE=%OSDKFILE% util\print util\screen
+SET OSDKFILE=%OSDKFILE% math\atan2 math\div math\norm math\root math\square
+SET OSDKFILE=%OSDKFILE% raster\buffer raster\filler raster\line raster\line8 raster\fill8
+SET OSDKFILE=%OSDKFILE% util\print util\screen util\display
 
 :: List of files to put in the DSK file.
 :: Implicitely includes BUILD/%OSDKNAME%.TAP
 SET OSDKTAPNAME="glOric"
 
-::run99.tap ..\intro\build\intro.tap 
+::run99.tap ..\intro\build\intro.tap
 SET OSDKDNAME=" -- glOric --"
 SET OSDKINIST="?\"V0.01\":WAIT20:!RUNME.COM"
-
