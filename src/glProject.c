85e385fdadc36d8a36ddd57e61c79ea3ad87c0a3
jbperin <jbperin@users.noreply.github.com>
2020-03-05 21:04:28 +0100

    prepare to boost drawing

--- src/glProject.c
+++ src/glProject.c
@@ -2,6 +2,15 @@
 
 
 #ifdef USE_REWORKED_BUFFERS
+extern signed char points3dX[];
+extern signed char points3dY[];
+extern signed char points3dZ[];
+extern signed char points2aH[];
+extern signed char points2aV[];
+extern signed char points2dH[];
+extern signed char points2dL[];
+extern unsigned char nbPoints;
+
 #ifdef USE_C_ARRAYSPROJECT
 void glProjectArrays(){
     unsigned char ii;
