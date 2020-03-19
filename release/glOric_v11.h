2d740af36a5482b1614d896058ce7bef8aa7866f
jbperin <jbperin@users.noreply.github.com>
2020-03-19 20:42:35 +0100

    clean assembly part

--- release/glOric_v11.h
+++ release/glOric_v11.h
@@ -2,16 +2,9 @@
 #ifndef GLORIC_H
 #define GLORIC_H
 
-
-
 #define SCREEN_WIDTH 40
 #define SCREEN_HEIGHT 26
-#define NB_MAX_POINTS 64
-#define NB_MAX_SEGMENTS 64
-#define NB_MAX_FACES 64
-#define NB_MAX_PARTICULES 64
-#define ADR_BASE_SCREEN 48000  
-#define ADR_BASE_LORES_SCREEN 48040  
+#define ADR_BASE_SCREEN 48000 
 #define HIRES_SCREEN_ADDRESS 0xA000
 
 #define SIZEOF_3DPOINT 4
@@ -20,15 +13,9 @@
 #define SIZEOF_2DPOINT 4
 #define SIZEOF_FACE 4
 
-#define COLUMN_OF_COLOR_ATTRIBUTE 2
 #define NB_LESS_LINES_4_COLOR 4
 
-
-
-
-extern void glProject (char *tabpoint2D, char *tabpoint3D, unsigned char nbPoints, unsigned char options);
-
- // Camera Position
+ // Camera Position use only low bytes
 extern int CamPosX;
 extern int CamPosY;
 extern int CamPosZ;
@@ -37,13 +24,13 @@ extern int CamPosZ;
 extern char CamRotZ;			// -128 -> 127 unit : 2PI/(2^8 - 1)
 extern char CamRotX;
 
-// Geometry buffers ;
+ // Geometry size
 extern unsigned char nbPoints;
 extern unsigned char nbFaces;
 extern unsigned char nbSegments;
 extern unsigned char nbParticules;
 
-
+ // Geometry buffers
 extern signed char points3dX[];
 extern signed char points3dY[];
 extern signed char points3dZ[];
@@ -60,7 +47,14 @@ extern unsigned char facesPt2[];
 extern unsigned char facesPt3[];
 extern unsigned char facesChar[];
 
-
+// Render buffer
 extern char          fbuffer[];  // frame buffer SCREEN_WIDTH * SCREEN_HEIGHT
+
+extern void glProjectArrays();
+extern void glDrawFaces();
+extern void glDrawSegments();
+extern void glDrawParticules();
+extern void initScreenBuffers();
+extern void buffer2screen(char *);
 
 #endif
