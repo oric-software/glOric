1bd157592189dcfa12644b6859c04e286783a624
jbperin <jbperin@users.noreply.github.com>
2020-03-20 10:19:07 +0100

    add comment to help code readibility

--- walkthrough.c
+++ walkthrough.c
@@ -1,12 +1,51 @@
+/* 
+    3D Walthrough Template on ORIC with glOric
+
+        by Jean-Baptiste PERIN
+
+
+          |----------------                                                   
+          |             //|                                                   
+          |           //  |                                                   
+          |         //    |                                                   
+          |       //      |                                                   
+ ---------------//        |                                                   
+ |              |         |                       *\                          
+ |              |         |                     //  \\\                       
+ |              |         |                   //       \\                     
+ |              |         |                  /           \\                   
+ |              |         |                //            /|                   
+ |              |         |               /            // |                   
+ |              |         |             // \\       ///   |                   
+ |              |         |           //     \    //      |          *        
+ |              |         |          /        \\//        |         //\\      
+ |              |         |         |       *   \        /         / /  \     
+ |              |         |         |     //    |      //         / /    \    
+ |              |         |         |   //      |    /            / /     \\  
+ |              |         |         |  /        |  //            /  /       \ 
+ |              |         |         |//         |//             --- /  ------\
+ |              |         |         /           /                  /// |      
+ |              |         |                                        /   |      
+ |              |       //                                             |      
+ |              |     //                                               |      
+ |              |    /                                                 |      
+ |              |  //                                                  *      
+ |               //                                                           
+ -------------- /                                                             
+                                                                              
+                                       
+*/                                                                            
+
+
 #include "lib.h"
 #include "release\glOric_v11.h"
 
-#define abs(x) 				(((x)<0)?-(x):(x))
+#define abs(x)                 (((x)<0)?-(x):(x))
 
 // Some objects for the game scene : Tree, House, Tower
 char geomPine [],  geomHouse [],  geomTower [];
 
-unsigned char running; // in game state: 1 = Running, 0 = Leave.
+unsigned char running; // game state: 1 = Running, 0 = Leave.
 
 // Add a shape to the game scene
 void addGeom(signed char X, signed char Y, signed char Z, unsigned char sizeX, unsigned char sizeY, unsigned char sizeZ, unsigned char orientation, char geom[]);
@@ -75,14 +114,18 @@ void gameLoop() {
 
         player ();
 
+        // project 3D points to 2D coordinates
         glProjectArrays();
 
+        // empty buffer
         initScreenBuffers();
 
+        // draw game scene's shapes in buffer
         glDrawFaces();
         glDrawSegments();
         glDrawParticules();
 
+        // update display with buffer
         buffer2screen((void*)0);
         sprintf(ADR_BASE_SCREEN, "(X=%d Y=%d Z=%d) [%d %d]", CamPosX, CamPosY, CamPosZ, CamRotZ, CamRotX);
     }
@@ -109,9 +152,9 @@ void player () {
         backward(); break;
     case 11:  // up
         forward(); break;
-    // case 80:  // P
+    // case 80:  // P        HEP !! DONT TOUCH THAT !!!
     //     CamPosZ += 1; break;
-    // case 59:  // ;
+    // case 59:  // ;       FORGET ABOUT IT !!
     //     CamPosZ -= 1; break;
     case 81:  // Q
         running = 0; break;
@@ -136,6 +179,7 @@ void player () {
 unsigned char isAllowedPosition(signed int X, signed int Y, signed int Z) {
     unsigned int aX = abs(X);
     unsigned int aY = abs(Y);
+    // Walls of house
     if ((aX <=13) && (aY <= 9)) {
         if ((aY <= 7) && (X > -7)) {
             return 1;
@@ -143,6 +187,7 @@ unsigned char isAllowedPosition(signed int X, signed int Y, signed int Z) {
             return 0;
         }
     }
+    // Walls of tower
     if ((X >=18) && (X <= 30) && (Y == -18)) return 0;
     if ((X ==18) && (Y >= -30) && (Y <= -18)) return 0;
     if ((X ==30) && (Y >= -30) && (Y <= -18)) return 0;
@@ -151,7 +196,6 @@ unsigned char isAllowedPosition(signed int X, signed int Y, signed int Z) {
 void forward() {
     signed int X, Y;
     X = CamPosX; Y = CamPosY;
-
     if (-112 >= CamRotZ) {
         CamPosX--;
     } else if ((-112 < CamRotZ) && (-80 >= CamRotZ)) {
@@ -177,8 +221,7 @@ void forward() {
 }
 void backward() {
     signed int X, Y;
-    X = CamPosX;
-    Y = CamPosY;
+    X = CamPosX; Y = CamPosY;
     if (-112 >= CamRotZ) {
         CamPosX++;
     } else if ((-112 < CamRotZ) && (-80 >= CamRotZ)) {
@@ -260,43 +303,54 @@ void shiftRight() {
  *  / /    / _ \ | | / _ \ | '__|/ __|
  * / /___ | (_) || || (_) || |   \__ \
  * \____/  \___/ |_| \___/ |_|   |___/
- */                                  
-#define INK_BLACK	0
-#define INK_RED		1
-#define INK_GREEN	2
-#define INK_YELLOW	3
-#define INK_BLUE	4
-#define INK_MAGENTA	5
-#define INK_CYAN	6
-#define INK_WHITE	7
-#define HIRES_50Hz	30
-#define TEXT_50Hz	26
+ */                        
+
+#define INK_BLACK    0
+#define INK_RED      1
+#define INK_GREEN    2
+#define INK_YELLOW   3
+#define INK_BLUE     4
+#define INK_MAGENTA  5
+#define INK_CYAN     6
+#define INK_WHITE    7
+
+#define HIRES_50Hz   30
+#define TEXT_50Hz    26
+
+#define TEXTURE_BLUE       'b'
+#define TEXTURE_GREEN      'g'
+#define TEXTURE_YELLOW     'y'
+#define TEXTURE_CYAN       'c'
+#define TEXTURE_MAGENTA    'm'
+#define TEXTURE_RED        'r'
+#define TEXTURE_GREENLIGHT 'f'
+
 
 unsigned char tab_color [] = {INK_CYAN, INK_YELLOW, INK_MAGENTA, INK_BLUE, INK_GREEN, INK_RED, INK_CYAN, INK_YELLOW} ;
 
 void prepare_colors() {
     int ii, jj;
 
-	for (ii = 0; ii<=SCREEN_HEIGHT-NB_LESS_LINES_4_COLOR ; ii++){
-		poke (ADR_BASE_SCREEN+(ii*SCREEN_WIDTH)+0,HIRES_50Hz);
-		fbuffer[ii*SCREEN_WIDTH]=HIRES_50Hz;
-		for (jj = 0; jj < 8; jj++) {
-			poke (HIRES_SCREEN_ADDRESS+((ii*8+jj)*SCREEN_WIDTH)+1, tab_color[jj]);
-			poke (HIRES_SCREEN_ADDRESS+((ii*8+jj)*SCREEN_WIDTH)+2, TEXT_50Hz);
-		}
-	}
+    for (ii = 0; ii<=SCREEN_HEIGHT-NB_LESS_LINES_4_COLOR ; ii++){
+        poke (ADR_BASE_SCREEN+(ii*SCREEN_WIDTH)+0,HIRES_50Hz);
+        fbuffer[ii*SCREEN_WIDTH]=HIRES_50Hz;
+        for (jj = 0; jj < 8; jj++) {
+            poke (HIRES_SCREEN_ADDRESS+((ii*8+jj)*SCREEN_WIDTH)+1, tab_color[jj]);
+            poke (HIRES_SCREEN_ADDRESS+((ii*8+jj)*SCREEN_WIDTH)+2, TEXT_50Hz);
+        }
+    }
 }
 void initColors(){
 
     prepare_colors();
-    //              CYAN, YELLO, MAGE, BLUE GREEN, RED, CYAN, YELLO
-    change_char('c', 0x7F, 0x00, 0x00, 0x7F, 0x00, 0x00, 0x7F, 0x00); // cyan
-	change_char('y', 0x00, 0x7F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x7F); // yellow
-	change_char('m', 0x00, 0x00, 0x7F, 0x00, 0x00, 0x7F, 0x00, 0x00); // magenta
-	change_char('r', 0x00, 0x55, 0x7F, 0x00, 0x55, 0x7F, 0x00, 0x00); // red
-	change_char('g', 0x55, 0xAA, 0x00, 0x00, 0x7F, 0x00, 0x55, 0xAA); // green
-	change_char('b', 0xAA, 0x00, 0x00, 0x7F, 0x00, 0x00, 0x55, 0x00); // blue
-	change_char('f', 0x00, 0xAA, 0x00, 0x55, 0x7F, 0x00, 0x00, 0xAA); // light green
+    //                              CYAN, YELLO, MAGE, BLUE GREEN, RED, CYAN, YELLO
+    change_char(TEXTURE_CYAN,       0x7F, 0x00, 0x00, 0x7F, 0x00, 0x00, 0x7F, 0x00);
+    change_char(TEXTURE_YELLOW,     0x00, 0x7F, 0x00, 0x00, 0x00, 0x00, 0x00, 0x7F);
+    change_char(TEXTURE_MAGENTA,    0x00, 0x00, 0x7F, 0x00, 0x00, 0x7F, 0x00, 0x00);
+    change_char(TEXTURE_RED,        0x00, 0x55, 0x7F, 0x00, 0x55, 0x7F, 0x00, 0x00);
+    change_char(TEXTURE_GREEN,      0x55, 0xAA, 0x00, 0x00, 0x7F, 0x00, 0x55, 0xAA);
+    change_char(TEXTURE_BLUE,       0xAA, 0x00, 0x00, 0x7F, 0x00, 0x00, 0x55, 0x00);
+    change_char(TEXTURE_GREENLIGHT, 0x00, 0xAA, 0x00, 0x55, 0x7F, 0x00, 0x00, 0xAA);
 }
 /*  __     _                          
  * / _\   | |__    __ _  _ __    ___  ___ 
@@ -305,112 +359,109 @@ void initColors(){
  * \__/   |_| |_| \__,_|| .__/  \___||___/
  *                      |_|               
  */
-#define TEXTURE_1 'b'
-#define TEXTURE_2 'g'
-#define TEXTURE_3 'y'
-#define TEXTURE_4 'c'
-#define TEXTURE_5 'm'
-#define TEXTURE_6 'r'
-#define TEXTURE_7 'f'
-
 #define TRUNC_HEIGHT 1
 #define PINE_WIDTH 1
 #define PINE_HEIGHT 3
-
-char geomPine []= {
-/* Nb Coords = */ 7,
-/* Nb Faces = */ 2,
-/* Nb Segments = */ 1,
-/* Nb Particules = */ 0,
-// Coord List : X, Y, Z, unused
-0,          0,          0,              0,
-0,          0,          TRUNC_HEIGHT,   0,
-PINE_WIDTH, 0,          TRUNC_HEIGHT,   0,
-0,          PINE_WIDTH, TRUNC_HEIGHT,   0,
--PINE_WIDTH, 0,         TRUNC_HEIGHT,   0,
-0,          -PINE_WIDTH,TRUNC_HEIGHT,   0,
-0,          0,          PINE_HEIGHT,    0,
+                                             //          6
+char geomPine []= {                          //          /\       
+                                             //         /| \      
+/* Nb Coords = */ 7,                         //        //   \\    
+/* Nb Faces = */ 2,                          //       //    \ \3   
+/* Nb Segments = */ 1,                       //      / /     \/   
+/* Nb Particules = */ 0,                     //     / /       \   
+                                             //    /  /        \  
+// Coord List : X, Y, Z, unused              //   /  /         \  
+0,          0,          0,              0,   //  4---/   1------2 
+0,          0,          TRUNC_HEIGHT,   1,   //     /  //|        
+PINE_WIDTH, 0,          TRUNC_HEIGHT,   2,   //     / /  |        
+0,          PINE_WIDTH, TRUNC_HEIGHT,   3,   //    ///   |        
+-PINE_WIDTH, 0,         TRUNC_HEIGHT,   4,   //    /     |        
+0,          -PINE_WIDTH,TRUNC_HEIGHT,   5,   //   5      |        
+0,          0,          PINE_HEIGHT,    6,   //          |        
+                                             //          0        
 // Face List : idxPoint1, idxPoint2, idxPoint3, character 
-3, 5, 6, TEXTURE_2,
-2, 4, 6, TEXTURE_2,
+3, 5, 6, TEXTURE_GREEN,
+2, 4, 6, TEXTURE_GREEN,
 // Segment List : idxPoint1, idxPoint2, character , unused
 0, 1, '|', 0,
 // Particule List : idxPoint1, character 
 };
 
-#define TOWER_HEIGHT 3
-char geomTower []= {
-/* Nb Coords = */ 8,
-/* Nb Faces = */ 6, //12,
-/* Nb Segments = */ 4,
-/* Nb Particules = */ 0,
-// Coord List : X, Y, Z, unused
--1, -1,             0, 0,
--1,  1,             0, 0,
- 1,  1,             0, 0,
- 1, -1,             0, 0,
--1, -1, TOWER_HEIGHT, 0,
--1,  1, TOWER_HEIGHT, 0,
- 1,  1, TOWER_HEIGHT, 0,
- 1, -1, TOWER_HEIGHT, 0,
-// Face List : idxPoint1, idxPoint2, idxPoint3, character 
-0, 1, 4, TEXTURE_4,
-1, 4, 5, TEXTURE_4,
-1, 2, 5, TEXTURE_1,
-2, 5, 6, TEXTURE_1, 
-3, 2, 7, TEXTURE_4,
-2, 7, 6, TEXTURE_4,
+#define TOWER_HEIGHT 3       
+char geomTower []= {           //           7---------------6
+/* Nb Coords = */ 8,           //           |             //|
+/* Nb Faces = */ 6,            //           |           //  |
+/* Nb Segments = */ 4,         //           |         //    |
+/* Nb Particules = */ 0,       //           |       //      |
+// Coord List : X, Y, Z, Id    //  4--------------5/        |
+-1, -1,             0, 0,      //  |        .     |         |
+-1,  1,             0, 1,      //  |        .     |         |
+ 1,  1,             0, 2,      //  |        .     |         |
+ 1, -1,             0, 3,      //  |        .     |         |
+-1, -1,  TOWER_HEIGHT, 4,      //  |        .     |         |
+-1,  1,  TOWER_HEIGHT, 5,      //  |        .     |         |
+ 1,  1,  TOWER_HEIGHT, 6,      //  |              |         |
+ 1, -1,  TOWER_HEIGHT, 7,      //  |        3.....|........ 2
+// Face List                   //  |              |       // 
+0, 1, 4, TEXTURE_CYAN,         //  |              |     //   
+1, 4, 5, TEXTURE_CYAN,         //  |              |    /     
+1, 2, 5, TEXTURE_BLUE,         //  |              |  //      
+2, 5, 6, TEXTURE_BLUE,         //  |               //        
+3, 2, 7, TEXTURE_CYAN,         //  0------------- 1          
+2, 7, 6, TEXTURE_CYAN,
 // Segment List : idxPoint1, idxPoint2, character, unused 
 0, 4, '|',       0,
 3, 7, '|',       0,
 2, 6, '|',       0,
 1, 5, '|',       0,
 };
-
-char geomHouse []= {
-/* Nb Coords = */ 10,
-/* Nb Faces = */ 11,
-/* Nb Segments = */ 14,
-/* Nb Particules = */ 0,
-// Coord List : X, Y, Z, unused
- 1, 1, 0, 0, 
--1, 1, 0, 0,
--1,-1, 0, 0,
- 1,-1, 0, 0,
- 1, 1, 2, 0, 
--1, 1, 2, 0,
--1,-1, 2, 0,
- 1,-1, 2, 0,
- 1, 0, 3, 0,
--1, 0, 3, 0,
+                                   //               9
+char geomHouse []= {               //              /  \          
+                                   //             /    \    
+/* Nb Coords = */ 10,              //            /      \   
+/* Nb Faces = */ 11,               //           /        \   
+/* Nb Segments = */ 14,            //          5         4  
+/* Nb Particules = */ 0,           //         /.        /|  
+                                   //        8 .       / |  
+// Coord List : X, Y, Z, unused    //       / \.      /  |  
+ 1, 1, 0, 0,                       //      /   \     /   |                            
+-1, 1, 0, 1,                       //     /    1\.. /. . 0                           
+-1,-1, 0, 2,                       //    /     / \ /    /                            
+ 1,-1, 0, 3,                       //   6     /   7    /                             
+ 1, 1, 2, 4,                       //   |    /    |   /                               
+-1, 1, 2, 5,                       //   |   /     |  /                               
+-1,-1, 2, 6,                       //   |  /      | /                                
+ 1,-1, 2, 7,                       //   | /       |/                                 
+ 1, 0, 3, 8,                       //   |/________|                                  
+-1, 0, 3, 9,                       //   2         3
 // Face List : idxPoint1, idxPoint2, idxPoint3, character 
- 0, 1, 5, TEXTURE_6,
- 0, 4, 5, TEXTURE_6,
- 3, 2, 6, TEXTURE_6,
- 6, 3, 7, TEXTURE_6,
- 1, 2, 6, TEXTURE_5,
- 1, 6, 5, TEXTURE_5,
- 5, 6, 9, TEXTURE_5,
- 4, 5, 9, TEXTURE_3,
- 4, 9, 8, TEXTURE_3,
- 7, 6, 9, TEXTURE_3,
- 7, 9, 8, TEXTURE_3,
+ 0, 1, 5, TEXTURE_RED,      // side
+ 0, 4, 5, TEXTURE_RED,
+ 3, 2, 6, TEXTURE_RED,
+ 6, 3, 7, TEXTURE_RED,
+ 1, 2, 6, TEXTURE_MAGENTA, // back
+ 1, 6, 5, TEXTURE_MAGENTA,
+ 5, 6, 9, TEXTURE_MAGENTA,
+ 4, 5, 9, TEXTURE_YELLOW,  // roof
+ 4, 9, 8, TEXTURE_YELLOW,
+ 7, 6, 9, TEXTURE_YELLOW,
+ 7, 9, 8, TEXTURE_YELLOW,
 // Segment List : idxPoint1, idxPoint2, character , unused
 0, 1, '-', 0,
 1, 2, '-', 0,
 2, 3, '-', 0,
 4, 5, '-', 0,
 6, 7, '-', 0,
-0, 4,'|', 0,
-1, 5,'|', 0,
-2, 6,'|', 0,
-3, 7,'|', 0,
-4, 8,'/', 0,
-7, 8,'/', 0,
-5, 9,'/', 0,
-6, 9,'/', 0,
-9, 8,'-', 0,
-};
+0, 4, '|', 0,
+1, 5, '|', 0,
+2, 6, '|', 0,
+3, 7, '|', 0,
+4, 8, '/', 0,
+7, 8, '/', 0,
+5, 9, '/', 0,
+6, 9, '/', 0,
+9, 8, '-', 0,
+}; 
 
 void addGeom(
     signed char   X,
