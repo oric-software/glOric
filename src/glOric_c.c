d3c20d0ddcae8fcc460f5d95ecfa4d9b4f730350
jbperin <jbperin@users.noreply.github.com>
2020-03-30 21:45:49 +0200

    add change char

--- src/glOric_c.c
+++ src/glOric_c.c
@@ -117,3 +117,20 @@ void waitkey(){
 #include "glDrawParticules.c"
 
 #include "buffer2screen.c"
+
+
+
+void change_char(char c, unsigned char patt01, unsigned char patt02, unsigned char patt03, unsigned char patt04, unsigned char patt05, unsigned char patt06, unsigned char patt07, unsigned char patt08) {
+    unsigned char* adr;
+    adr      = (unsigned char*)(0xB400 + c * 8);
+    *(adr++) = patt01;
+    *(adr++) = patt02;
+    *(adr++) = patt03;
+    *(adr++) = patt04;
+    *(adr++) = patt05;
+    *(adr++) = patt06;
+    *(adr++) = patt07;
+    *(adr++) = patt08;
+}
+
+
